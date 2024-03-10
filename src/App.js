546ef8fb5bdb23cc5ac01d8af63ef0b249557dc8
TheHimanshuDixit <himanshuharshdixit@gmail.com>
2024-03-10 16:31:24 +0530

    navigation top fixed

--- src/App.js
+++ src/App.js
@@ -11,22 +11,24 @@ import Team from "./components/team";
 import About from "./components/about";
 import Contribute from "./components/contribute";
 import Error from "./components/error";
+import ScrollToTop from "./components/scrollTotop";
 
 function App() {
   return (
     <div className="bg-gradient-to-t from-slate-50 to-purple-100">
       <BrowserRouter>
-      <Header />
+        <Header />
+        <ScrollToTop />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/openings" element={<Openings/>} />
-          <Route exact path="/contact" element={<Contact/>} />
-          <Route exact path="/team" element={<Team/>} />
-          <Route exact path="/about" element={<About/>} />
-          <Route exact path="/contribute" element={<Contribute/>} />
-          <Route path="*" element={<Error/>} />
+          <Route exact path="/openings" element={<Openings />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/team" element={<Team />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contribute" element={<Contribute />} />
+          <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
       </BrowserRouter>
