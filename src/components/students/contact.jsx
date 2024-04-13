4460603a9c61b68c00c2904087c3f86b75eabeb6
TheHimanshuDixit <himanshuharshdixit@gmail.com>
2024-04-13 20:18:53 +0530

    contact page Integrated

--- src/components/students/contact.jsx
+++ src/components/students/contact.jsx
@@ -1,10 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { initTE, Input, Ripple } from "tw-elements";
 
 const Contact = () => {
   useEffect(() => {
     initTE({ Input, Ripple });
   }, []);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "name") setName(value);
+    if (name === "email") setEmail(value);
+    if (name === "message") setMessage(value);
+  };
+
+  const handleSubmit = async () => {
+    const response = await fetch("/api/contact/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, message }),
+    });
+    const data = await response.json();
+    if (data.message === "Email sent") alert("Email sent");
+    else alert("Error Occurs");
+    console.log(data);
+  };
+
   return (
     <div>
       <div className="w-3/4 my-24 mx-auto md:px-6">
@@ -60,6 +86,9 @@ const Contact = () => {
                 <div className="relative mb-6" data-te-input-wrapper-init>
                   <input
                     type="text"
+                    name="name"
+                    value={name}
+                    onChange={handleChange}
                     className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                     id="exampleInput90"
                     placeholder="Name"
@@ -73,6 +102,9 @@ const Contact = () => {
                 <div className="relative mb-6" data-te-input-wrapper-init>
                   <input
                     type="email"
+                    name="email"
+                    value={email}
+                    onChange={handleChange}
                     className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                     id="exampleInput91"
                     placeholder="Email address"
@@ -85,10 +117,14 @@ const Contact = () => {
                 </div>
                 <div className="relative mb-6" data-te-input-wrapper-init>
                   <textarea
+                    name="message"
+                    onChange={handleChange}
                     className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                     id="exampleFormControlTextarea1"
                     rows="3"
-                    placeholder="Your message"></textarea>
+                    placeholder="Your message">
+                    {message}
+                  </textarea>
                   <label
                     htmlFor="exampleFormControlTextarea1"
                     className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">
@@ -97,6 +133,7 @@ const Contact = () => {
                 </div>
                 <button
                   type="button"
+                  onClick={handleSubmit}
                   data-te-ripple-init
                   data-te-ripple-color="light"
                   className="mb-6 inline-block w-full rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
