383b5daf288816b467c1aa97c257998654603026
TheHimanshuDixit <himanshuharshdixit@gmail.com>
2024-04-26 17:07:09 +0530

    signup bug fixes

--- src/components/students/signup.jsx
+++ src/components/students/signup.jsx
@@ -169,7 +169,7 @@ const Signup = () => {
                     <a
                       href="#!"
                       className="text-danger transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700">
-                      Register
+                      Create
                     </a>
                   </p>
                 </div>
